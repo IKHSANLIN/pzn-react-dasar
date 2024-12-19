b4c17e76262617f8c677b6bce030588676346a4a
Ikhsan Lintang Prakasa <ikhsanlintang06@gmail.com>
2024-12-19 14:18:21 +0700

    Collection Component

--- src/todolist/TodoList.jsx
+++ src/todolist/TodoList.jsx
@@ -1,13 +1,34 @@
 import Todo from "./Todo";
 
 export default function TodoList() {
+  const data = [
+    {
+      id: 0,
+      text: "Belajar React",
+      isComplited: true,
+    },
+    {
+      id: 1,
+      text: "Belajar Vue",
+      isComplited: true,
+    },
+    {
+      id: 2,
+      text: "Belajar Angular",
+      isComplited: false,
+    },
+    {
+      id: 3,
+      text: "Belajar Svelte",
+      isComplited: true,
+    },
+  ];
+  //   const todos = data.map((todo) => <Todo {...todo} key={todo.id} />);
   return (
     <ul>
-      <Todo text="Belajar React" isComplited={true} />
-      <Todo text="Belajar Vue" isComplited={false} />
-      <Todo text="Belajar Angular" isComplited={false} />
-      <Todo text="Belajar Svelte" isComplited={true} />
-      <Todo text="Belajar Preact" isComplited={true} />
+      {data.map((todo) => (
+        <Todo {...todo} key={todo.id} />
+      ))}
     </ul>
   );
 }
