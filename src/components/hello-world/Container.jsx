81353d318b52b005f5b1837cf82db376de4ecd44
Ikhsan Lintang Prakasa <ikhsanlintang06@gmail.com>
2024-12-19 09:52:34 +0700

    Nested Component

---
+++ src/components/hello-world/Container.jsx
@@ -0,0 +1,11 @@
+export default function Container({ children }) {
+  return (
+    <div>
+      <h1>Ikhsan Lintang</h1>
+      {children}
+      <footer>
+        <p>Jr. Programmers</p>
+      </footer>
+    </div>
+  );
+}
--- src/components/hello-world/main.jsx
+++ src/components/hello-world/main.jsx
@@ -1,4 +1,12 @@
 import { createRoot } from "react-dom/client";
 import HelloWorld from "./HelloWorld";
+import { StrictMode } from "react";
+import Container from "./Container";
 
-createRoot(document.getElementById("root")).render(<HelloWorld />);
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <Container>
+      <HelloWorld />
+    </Container>
+  </StrictMode>
+);
