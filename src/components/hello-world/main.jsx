93240c2ecf83c203e938c302ea4d74c6ed813fd4
Ikhsan Lintang Prakasa <ikhsanlintang06@gmail.com>
2024-12-19 20:55:06 +0700

    Event In Props

--- src/components/hello-world/main.jsx
+++ src/components/hello-world/main.jsx
@@ -5,6 +5,7 @@ import Container from "./Container";
 import TodoList from "../../todolist/TodoList";
 import Table from "../../table/Table";
 import AlertButton from "../button/AlertButton";
+import MyButton from "../button/MyButton";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -13,7 +14,7 @@ createRoot(document.getElementById("root")).render(
       <TodoList />
       <Table />
       <AlertButton text={"Click me"} message={"Hello World"} />
-      <AlertButton text={"Click me"} message={"Hello Ikhsan"} />
+      <MyButton text={"Event In Props"} onSmash={() => alert("Hello World")} />
     </Container>
   </StrictMode>
 );
