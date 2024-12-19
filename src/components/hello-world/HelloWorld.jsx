3ff098c75d3c32f3e58596342e2a37d1789f8de3
Ikhsan Lintang Prakasa <ikhsanlintang06@gmail.com>
2024-12-19 10:53:08 +0700

    style

--- src/components/hello-world/HelloWorld.jsx
+++ src/components/hello-world/HelloWorld.jsx
@@ -1,30 +1,17 @@
+import "./HelloWorld.css";
 export function HeaderHelloWorld({ text = "Lupa kasih text" }) {
-  return <h1 style={{ color: "green" }}>{text.toUpperCase()}</h1>;
+  return <h1 className="title">{text.toUpperCase()}</h1>;
 }
 
 export function ParagraphHelloWorld() {
   const text = "Belajar React JS Dasar PZN";
-  return (
-    <p
-      style={{
-        color: "red",
-        backgroundColor: "aqua",
-        width: 200,
-        padding: 20,
-        margin: 0,
-        display: "flex",
-        justifyContent: "center",
-      }}
-    >
-      {text.toLowerCase()}
-    </p>
-  );
+  return <p className="content">{text.toLowerCase()}</p>;
 }
 
 export default function HelloWorld() {
-  const props ={
-    text: "Hello World pzn"
-  }
+  const props = {
+    text: "Hello World pzn",
+  };
   return (
     <div>
       <HeaderHelloWorld {...props} />
